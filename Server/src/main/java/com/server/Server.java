0c6703b85ccfee52a5288fc06e314d3890d89144
ProgrammerTony <tony.789@live.com>
2022-10-20 10:55:40 -0400

    User model and credentials added along with a job scheduling service

--- Server/src/main/java/com/server/Server.java
+++ Server/src/main/java/com/server/Server.java
@@ -1,19 +1,28 @@
 package com.server;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.server.model.Message;
+import com.server.core.concurrent.JobScheduler;
+import com.server.core.concurrent.impl.HelloWorldJob;
 
 @SpringBootApplication
 public class Server {
 
+	/**
+	 * A logger used to report messages.
+	 */
+	private static Logger logger = Logger.getLogger(Server.class.getName());
+	
 	public static void main(String[] args) {
+		logger.info("Initializing gambit server...");
+		
+		JobScheduler scheduler = new JobScheduler();
+	
+		scheduler.schedule(new HelloWorldJob());
+		
 		SpringApplication.run(Server.class, args);
 	}
 
--- Server/src/main/java/com/server/model/User.java
+++ Server/src/main/java/com/server/model/User.java
@@ -8,12 +8,21 @@ package com.server.model;
 
 public class User {
 	
+	/**
+	 * LEague of legends unique global identification digit
+	 */
+	private long puuid;
 
 	/**
 	 * The name the user is addressed by
 	 */
 	private String username;
 	
+	/**
+	 * The user credentials that contains their login details.
+	 */
+	private Credentials credentials;
+
 	/**
 	 * Creates a user
 	 * @param username the name the user is addressed by
@@ -22,12 +31,33 @@ public class User {
 		this.username = username;
 	}
 	
+	public User(String username, Credentials credentials) {
+		this(username);
+		this.credentials = credentials;
+	}
+	
 	/**
 	 * Gets the username of the user
 	 * @return username
 	 */
 	public String getUsername() {
 		return username;
+	}
+
+	/**
+	 * Gets the league of legends unique identification digit
+	 * @return {@code puuid}
+	 */
+	public long getPuuid() {
+		return puuid;
+	}
+
+	/**
+	 * Modifies the league of legends unique identification digit
+	 * @param puuid the updated unique identification digit
+	 */
+	public void setPuuid(long puuid) {
+		this.puuid = puuid;
 	}
 	
  }
