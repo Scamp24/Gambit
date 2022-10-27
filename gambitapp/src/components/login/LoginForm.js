cc66f0cf64381f01031525be99f2c17280656eb9
ProgrammerTony <tony.789@live.com>
2022-10-27 16:27:29 -0400

    Simplified Login Form
    Simple login form using SASS and Bootstrap components

--- gambitapp/src/components/login/LoginForm.js
+++ gambitapp/src/components/login/LoginForm.js
@@ -1,68 +1,29 @@
 import "../../css/style.css";
-import { useState } from "react";
-import {Col, Button, Row, Container, Card, Form} from 'react-bootstrap';
+import { useState, Component } from "react";
+import {Container, InputGroup, Form, Row, Col, Button} from 'react-bootstrap';
 
-const LoginForm = () => {
-    return (
+export class LoginForm extends Component {
+    render() {
+        return (
 
             <Container>
-                <Row className="vh-100 d-flex justify-content-center align-items-center">
-                    <Col md={9} lg={8} xs={12}>
-                        <div className="border border-3 border-primary"></div>
-                        <Card className="shadow form">
-                            <Card.Body>
-                                <div className="mb-3 mt-md-4">
-                                    <h2 className="fw-bold mb-2 text-uppercase ">Gambit LFG</h2>
-                                    <p className=" mb-5">Please enter your login and password!</p>
-                                    <div className="mb-3">
-                                        <Form>
-                                            <Form.Group className="mb-3 form__input" controlId="formBasicEmail">
-                                                <Form.Label className="text-center">
-                                                Email address
-                                                </Form.Label>
-                                                <Form.Control type="email" placeholder="Enter email" />
-                                            </Form.Group>
-
-                                            <Form.Group
-                                                className="mb-3 form__input"
-                                                controlId="formBasicPassword"
-                                            >
-                                                <Form.Label>Password</Form.Label>
-                                                <Form.Control type="password" placeholder="Password" />
-                                            </Form.Group>
-                                            <Form.Group
-                                                className="mb-3"
-                                                controlId="formBasicCheckbox"
-                                            >
-                                                <p className="small">
-                                                <a className="text-primary" href="#!">
-                                                    Forgot password?
-                                                </a>
-                                                </p>
-                                            </Form.Group>
-                                            <div className="d-grid">
-                                                <Button variant="primary" type="submit">
-                                                Login
-                                                </Button>
-                                            </div>
-                                        </Form>
-                                        <div className="mt-3">
-                                            <p className="mb-0  text-center">
-                                                Don't have an account?{" "}
-                                                <a href="{''}" className="text-primary fw-bold">
-                                                Sign Up
-                                                </a>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                <Row className="d-flex justify-content-center">
+                    <Form className="form__login u-center-text">
+                        <InputGroup className="form__login-input-group">
+                            <InputGroup.Text id="email_field">E-Mail</InputGroup.Text>
+                            <Form.Control placeholder="email" aria-label="email" aria-describedby="email"/>
+                        </InputGroup>
+                        <InputGroup className="form__login-input-group">
+                            <InputGroup.Text id="password_field">Password</InputGroup.Text>
+                            <Form.Control placeholder="password" aria-label="password" aria-describedby="password"/>
+                        </InputGroup>
+                        <Button variant="success">Login</Button>{' '}
+                    </Form>
                 </Row>
             </Container>
-    
-    )
+        
+        );
+    }
 }
 
-export default LoginForm
+export default LoginForm;
